c1ba43ce75dd61ead2da31d2614b2ec41b751f67
Vinicius Marques de Oliveira <76068967+Marquezv@users.noreply.github.com>
2023-01-25 22:15:38 -0300

    Add files via upload

--- db_function.py
+++ db_function.py
@@ -1,5 +1,6 @@
 from sqlmodel import Session, select
-from models import Pessoa, engine
+from sqlalchemy.orm import joinedload
+from models import Pessoa, Livro, engine
 
 
 def create_pessoa(idade: int, nome: str):
@@ -16,6 +17,28 @@ def all_pessoa() -> list[Pessoa]:
     query = select(Pessoa)
     with Session(engine) as session:
         result = session.execute(query).scalars().all()
+
+    return result
+
+def create_livro(titulo: str, pessoa_id: int):
+    livro = Livro(titulo=titulo, pessoa_id=pessoa_id)
+    with Session(engine) as session:
+        session.add(livro)
+        session.commit()
+        session.refresh(livro)
+
+    return livro
+
+
+
+def all_livro() -> list[Livro]:
+    query = select(Livro).options(joinedload('*'))
+    with Session(engine) as session:
+        result = session.execute(query).scalars().unique().all()
+
+    # Force eager
+    # import pdb; pdb.set_trace();
+    # Nao faca isso
 
     return result
 
--- schema.py
+++ schema.py
@@ -3,7 +3,9 @@ from typing import Optional, List
 from strawberry.fastapi import GraphQLRouter
 from db_function import (
     create_pessoa,
-    all_pessoa
+    all_pessoa,
+    create_livro,
+    all_livro
 )
 
 @strawberry.type
@@ -11,15 +13,24 @@ class Pessoa:
     id: Optional[int]
     nome: str
     idade: int
+    livros: List['Livro']
+
+@strawberry.type
+class Livro:
+    id: Optional[int]
+    titulo: str
+    pessoa: Pessoa
+
 
 @strawberry.type
 class Query:
     all_pessoa: List[Pessoa] = strawberry.field(resolver=all_pessoa)
+    all_livro: List[Livro] = strawberry.field(resolver=all_livro)
 
 @strawberry.type
 class Mutation:
     create_pessoa: Pessoa = strawberry.field(resolver=create_pessoa)
-
+    create_livro: Livro = strawberry.field(resolver=create_livro)
 
 schema = strawberry.Schema(
     query=Query,
